3afcc16740c064e43bb75e8b0bec52d59d3af7a5
ganezasan <istaka12@gmail.com>
2020-10-02 09:15:57 +0900

    update test

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -5,25 +5,10 @@ module.exports = {
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .waitForElementVisible('input[name=btnK]', 1000)
+      .click('input[name=btnK]')
       .pause(1000)
       .assert.containsText('#main', 'nightwatch')
       .end();
   }
 };
-
-// module.exports = {
-//   'Demo test ecosia.org' : function(browser) {
-//     browser
-//       .url('https://www.ecosia.org/')
-//       .waitForElementVisible('body')
-//       .assert.titleContains('Ecosia')
-//       .assert.visible('input[type=search]')
-//       .setValue('input[type=search]', 'nightwatch')
-//       .assert.visible('button[type=submit]')
-//       .click('button[type=submit]')
-//       .assert.containsText('.mainline-results', 'Nightwatch.js')
-//       .end();
-//   }
-// };
