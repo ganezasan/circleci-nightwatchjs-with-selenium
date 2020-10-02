ca6067b738442c6795db02bc939cbcf5f845c041
ganezasan <istaka12@gmail.com>
2020-10-02 09:06:26 +0900

    update

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,14 +1,29 @@
 module.exports = {
-  'Demo test ecosia.org' : function(browser) {
+  'Demo test Google' : function (browser) {
     browser
-      .url('https://www.ecosia.org/')
-      .waitForElementVisible('body')
-      .assert.titleContains('Ecosia')
-      .assert.visible('input[type=search]')
-      .setValue('input[type=search]', 'nightwatch')
-      .assert.visible('button[type=submit]')
-      .click('button[type=submit]')
-      .assert.containsText('.mainline-results', 'Nightwatch.js')
+      .windowMaximize()
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'nightwatch')
       .end();
   }
 };
+
+// module.exports = {
+//   'Demo test ecosia.org' : function(browser) {
+//     browser
+//       .url('https://www.ecosia.org/')
+//       .waitForElementVisible('body')
+//       .assert.titleContains('Ecosia')
+//       .assert.visible('input[type=search]')
+//       .setValue('input[type=search]', 'nightwatch')
+//       .assert.visible('button[type=submit]')
+//       .click('button[type=submit]')
+//       .assert.containsText('.mainline-results', 'Nightwatch.js')
+//       .end();
+//   }
+// };
